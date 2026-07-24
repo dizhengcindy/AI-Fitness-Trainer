3e488f5360e263ba4ad0d10533ea096f3111ccd5
dizhengcindy <dizhengcindy@gmail.com>
2026-07-24 13:54:00 -0500

    set up webhooks

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Next.js project
 
 use https://ui.shadcn.com/ as UI components library
 
-use clerk for authentication and user management
+use clerk for authentication and user management, clerk use svix to send webhooks
 
 Vapi for AI conversation
 
---
+++ convex/users.ts
@@ -0,0 +1,21 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const syncUser = mutation({
+  args: {
+    email: v.string(),
+    name: v.string(),
+    image: v.optional(v.string()),
+    clerkId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    if (existingUser) return;
+
+    return await ctx.db.insert("users", args);
+  },
+});
