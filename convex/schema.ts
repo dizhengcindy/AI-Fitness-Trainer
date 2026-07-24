b60bec27079bac7d3acafe84af5b81b393fd3300
dizhengcindy <dizhengcindy@gmail.com>
2026-07-24 14:37:01 -0500

    add nav bar

--- convex/schema.ts
+++ convex/schema.ts
@@ -13,27 +13,34 @@ export default defineSchema({
     userId: v.id("users"),
     name: v.string(),
     workoutPlan: v.object({
-        schedule: v.array( v.string()),
-        exercises: v.array(v.object({
-            day: v.string(),
-            routines: v.array(v.object({
-                name: v.string(),
-                sets: v.optional(v.number()),
-                reps: v.optional(v.number()),
-                duration: v.optional(v.number()),
-                description: v.optional(v.string()),
-                exercises: v.optional(v.array(v.string())),
-            })),
-        })),
+      schedule: v.array(v.string()),
+      exercises: v.array(
+        v.object({
+          day: v.string(),
+          routines: v.array(
+            v.object({
+              name: v.string(),
+              sets: v.optional(v.number()),
+              reps: v.optional(v.number()),
+              duration: v.optional(v.number()),
+              description: v.optional(v.string()),
+              exercises: v.optional(v.array(v.string())),
+            }),
+          ),
+        }),
+      ),
     }),
     dietPlan: v.object({
       dailyCalories: v.number(),
-      meals: v.array(v.object({
-        name: v.string(),
-        foods: v.array(v.string())
-      })),
+      meals: v.array(
+        v.object({
+          name: v.string(),
+          foods: v.array(v.string()),
+        }),
+      ),
     }),
     isActive: v.boolean(),
-  }).index("by_user_id", ["userId"]).index("by_is_active", ["isActive"]),
-
+  })
+    .index("by_user_id", ["userId"])
+    .index("by_is_active", ["isActive"]),
 });
